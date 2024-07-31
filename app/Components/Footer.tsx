bbcbdb3b0eca8297543313ed919664fbe49a6d6d
nidu003 <108021671+nidu003@users.noreply.github.com>
2024-07-31 16:12:49 +0530

    Update Footer component, globals.css, and add footer image

--- app/Components/Footer.tsx
+++ app/Components/Footer.tsx
@@ -1,45 +1,19 @@
-import Head from 'next/head';
 import Image from 'next/image';
-import Link from 'next/link';
 
-export default function Footer() {
+function Footer() {
   return (
-    <>
-      <Head>
-        <title>Aakash Pikalmonde</title>
-        <meta name="description" content="Aakash Pikalmonde's portfolio" />
-        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-      </Head>
-
-      <div className="footer-main-container">
-        <div className="footer">
-          <h1>AAKASH</h1>
-          <h2>PIKALMUNDE</h2>
-          <h3>REACH SOCIAL</h3>
-          <p className='p1'>
-          Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam.
-        </p>
-        <p className='p2'>
-          Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam.
-        </p>
-
-
-          <div className="social-icons">
-            <Link href="#"><Image src="/facebook.svg" alt="Facebook" width={50} height={50} /></Link>
-            <Link href="#"><Image src="/youtube.svg" alt="YouTube" width={50} height={50} /></Link>
-            <Link href="#"><Image src="/instagram.svg" alt="Instagram" width={50} height={50} /></Link>
-            <Link href="#"><Image src="/twitter.svg" alt="Twitter" width={50} height={50} /></Link>
-          </div>
-
-          <div className="contact">
-            CONTACT FOR COLLABORATION: <a href="mailto:collab@gmail.com">collab@gmail.com</a>
-          </div>
-
-          <div className="copyright">
-            COPYRIGHT © 2023 GBJ BUZZ PVT LTD. ALL RIGHTS RESERVED.
-          </div>
-        </div>
-      </div>
-    </>
+    <div className="full-screen-image-container">
+      <Image
+        src="/footer.jpg"
+        layout="responsive"
+        width={1920}
+        height={1500}
+        objectFit="cover"
+        key="footer-image"
+        alt="Footer"
+      />
+    </div>
   );
 }
+
+export default Footer;
