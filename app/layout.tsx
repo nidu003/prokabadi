ab88b82e7d57e7412b5cde5687693fe642e88288
nidu003 <108021671+nidu003@users.noreply.github.com>
2024-07-31 17:21:41 +0530

    Describe your changes here

--- app/layout.tsx
+++ app/layout.tsx
@@ -12,7 +12,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" className="overflow-x-hidden">
       <body className="min-h-screen flex flex-col">
         <main className="flex-grow">{children}</main>
       </body>
